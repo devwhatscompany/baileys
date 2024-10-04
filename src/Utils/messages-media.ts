88fb96e17f8be372c9219b8203c61c443766a27f
Samuel Rodrigues Almeida Costa <devsamuelrac@gmail.com>
2024-10-04 10:52:35 -0300

    teste

--- src/Utils/messages-media.ts
+++ src/Utils/messages-media.ts
@@ -1,5 +1,5 @@
 import { Boom } from '@hapi/boom'
-import { AxiosRequestConfig } from 'axios'
+import axios, { AxiosRequestConfig } from 'axios'
 import { exec } from 'child_process'
 import * as Crypto from 'crypto'
 import { once } from 'events'
@@ -324,7 +324,7 @@ export async function generateThumbnail(
 }
 
 export const getHttpStream = async(url: string | URL, options: AxiosRequestConfig & { isStream?: true } = {}) => {
-	const { default: axios } = await import('axios')
+	// const { default: axios } = await import('axios')
 	console.log('TO AQUI getHttpStream')
 	const fetched = await axios.get(url.toString(), { ...options, responseType: 'stream' })
 	return fetched.data as Readable
