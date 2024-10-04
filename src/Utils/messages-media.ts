32144f728947ea66297ae39a456a1b63c023b4bc
Samuel Rodrigues Almeida Costa <devsamuelrac@gmail.com>
2024-10-04 10:42:35 -0300

    teste 2

--- src/Utils/messages-media.ts
+++ src/Utils/messages-media.ts
@@ -325,7 +325,7 @@ export async function generateThumbnail(
 
 export const getHttpStream = async(url: string | URL, options: AxiosRequestConfig & { isStream?: true } = {}) => {
 	const { default: axios } = await import('axios')
-	const fetched = await axios.get(url.toString(), { ...options, responseType: 'stream' })
+	const fetched = await axios.get(url.toString(), { ...options, responseType: 'arraybuffer' })
 	return fetched.data as Readable
 }
 
